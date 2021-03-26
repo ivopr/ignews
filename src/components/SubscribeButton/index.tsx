f19b6e15032332257805275843f73a80e5dc9c0e
Ivo <ivoprovensi1@gmail.com>
2021-03-26 19:34:48 -0400

    ops, esqueci de commitar o resto

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,47 @@
+import { signIn, useSession } from "next-auth/client"
+import { useRouter } from "next/router"
+import { api } from "../../services/api"
+import { getStripeJs } from "../../services/stripejs"
+
+import styles from "./styles.module.scss"
+
+interface SubscribeButtonProps {
+  priceId: string
+}
+
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession()
+  const router = useRouter()
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn("github")
+      return
+    }
+
+    if (session?.activeSubscription) {
+      router.push("/posts")
+      return
+    }
+
+    try {
+      const response = await api.post("/subscribe")
+      const { sessionId } = response.data
+      const stripe = await getStripeJs()
+
+      await stripe.redirectToCheckout({ sessionId })
+    } catch (error) {
+      alert(error.message)
+    }
+  }
+
+  return (
+    <button
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+      type="button"
+    >
+      Subscribe Now
+    </button>
+  )
+}
